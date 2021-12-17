1557791df1048e4f1775dbf59b6d48328b4b29c2
王宏亮 <401765060@qq.com>
2021-12-17 19:46:59 +0800

    新增麦克风实时驱动

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
     <button @click="say()">文字驱动数字人</button>
-    <button @click="unmute()">取消静音</button>
+    <button @click="tooglemute()">静音/取消</button>
     <button @click="reload()">重新加载</button>
     <button @mousedown="startRec" @mouseup="finishRec">{{ recText }}</button>
+    <button @click="toogleLive">{{ liveText }}</button>
     <div class="dt"></div>
   </div>
 </template>
@@ -18,6 +19,8 @@ export default {
     return {
       isRecInited: false,
       recText: '按下录音',
+      liveText: '开始麦克风实时驱动',
+      emuted: true,
     }
   },
   created() {},
@@ -29,12 +32,17 @@ export default {
         robotMode: 'drive',
         robotCode: '245982983437047536',
       })
+
+      this.duix.on('detectedSpeech', (text) => {
+        console.log(text)
+        this.speak(text)
+      })
     },
     say() {
       this.duix.speak('你好，欢迎使用硅基智能的数字人对话产品。')
     },
-    unmute() {
-      this.duix.setVideoMuted(false)
+    tooglemute() {
+      this.duix.setVideoMuted(!this.emuted)
     },
     reload() {
       this.duix.stop()
@@ -50,6 +58,17 @@ export default {
     finishRec() {
       this.recText = '按下录音'
       this.duix.stopRecord()
+    },
+    toogleLive() {
+      if (this.living) {
+        this.living = false
+        this.duix.closeLive()
+        this.liveText = '开始麦克风实时驱动'
+      } else {
+        this.living = true
+        this.duix.startLive()
+        this.liveText = '关闭麦克风实时驱动'
+      }
     },
   },
   mounted() {
